c3e7e2b1e01fcd0e0e7963b7ff0067e626749827
test Chan <test371912@gmail.com>
2023-05-24 14:40:31 +0000

    Fetch the name value from previous page

--- webapp/controller/List.controller.js
+++ webapp/controller/List.controller.js
@@ -1,20 +1,24 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/UIComponent"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller,UIComponent) {
         "use strict";
 
         return Controller.extend("grocery.controller.List", {
             onInit: function () {
                 var oRouter = UIComponent.getRouterFor(this);
-                oRouter.getRoute("RouteDashboard").attachPatternMatched(this.onRouteMatched, this)
+                oRouter.getRoute("RouteList").attachPatternMatched(this.onRouteMatched, this)
             },
             onRouteMatched:function(oEvent){
+                debugger
+                let welcome="Welcome "
                 let name=oEvent.getParameter("arguments").name
-                this.getView().by
+                let newMessage=welcome+name+ "!"
+                this.getView().byId("nameLabel").setProperty("text",newMessage)
             }
         });
     });
