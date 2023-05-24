68fcf82d2f189abe3ec410cc570dcdd216c243a0
test Chan <test371912@gmail.com>
2023-05-24 14:11:06 +0000

    Enable/Disable button based input

--- webapp/controller/Dashboard.controller.js
+++ webapp/controller/Dashboard.controller.js
@@ -10,6 +10,26 @@ sap.ui.define([
         return Controller.extend("grocery.controller.Dashboard", {
             onInit: function () {
 
+            },
+            setNewName:function(){
+                let currName=this.getView().byId("nameInput").getValue();
+                let buttonReference=this.getView().byId("nameSubmit");
+                let currNameLength=currName.length;
+                //Case 1: Accept if there are input characters
+                //Case 2: Reject if there are no input characters
+                if(currNameLength>0){
+                    buttonReference.setProperty("text", "Accept");
+                    buttonReference.setProperty("type", "Accept");
+                    buttonReference.setProperty("icon", "sap-icon://accept");
+                    buttonReference.setProperty("enabled", true);
+                }
+                else{
+                    buttonReference.setProperty("text", "Reject")
+                    buttonReference.setProperty("type", "Reject");
+                    buttonReference.setProperty("icon", "sap-icon://decline")
+                    buttonReference.setProperty("enabled", false)
+                }
             }
+
         });
     });
